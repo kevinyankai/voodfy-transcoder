befbe5c868efc885798ea2274de295cece18831a
Leandro Barbosa <leandrobar93@gmail.com>
2020-07-24 13:25:20 -0300

    sending token and address used on powergate instace

--- pkg/powergate/powergate.go
+++ pkg/powergate/powergate.go
@@ -45,8 +45,8 @@ func FFSCreate() (string, string, error) {
 }
 
 // FFSAuthenticate return a authenticate client
-func FFSAuthenticate(token string) *pow.Client {
-	ma, err := multiaddr.NewMultiaddr("/ip4/127.0.0.1/tcp/5002")
+func FFSAuthenticate(token, address string) *pow.Client {
+	ma, err := multiaddr.NewMultiaddr(address)
 	checkErr(err)
 
 	var opts []grpc.DialOption
@@ -61,18 +61,18 @@ func FFSAuthenticate(token string) *pow.Client {
 func FFSDefaultConfig(token string) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
 	defer cancel()
-	client := FFSAuthenticate(token)
+	client := FFSAuthenticate(token, "/ip4/127.0.0.1/tcp/5002")
 	conf, err := client.FFS.DefaultConfig(authCtx(ctx, token))
 	checkErr(err)
 	log.Println(fmt.Sprintf("Configuration %v", conf))
 }
 
 // FFSPush send the cid to powergate setup the hot and cold configuration
-func FFSPush(cidHash, token string) string {
+func FFSPush(cidHash, token, address string) string {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
 	defer cancel()
 
-	fClient := FFSAuthenticate(token)
+	fClient := FFSAuthenticate(token, address)
 
 	c, err := cid.Parse(cidHash)
 	checkErr(err)
